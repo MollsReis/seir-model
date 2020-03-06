e84170890fcac1de2c1e57b387e38c61059b8c15
EvilScott <reis.robert.s@gmail.com>
2020-03-06 14:59:14 -0800

    refactor chart to be more dynamic

--- index.js
+++ index.js
@@ -1,69 +1,58 @@
-const generateModel = (inf, inc, rec) => {
-  const n = 100,
-        s = [{ x: 0, y: 100 }],
-        e = [{ x: 0, y: 0 }],
-        i = [{ x: 0, y: 1 }],
-        r = [{ x: 0, y: 0 }];
-  for (let x = 1; x < 200; x++) { // TODO run until i = 0
-    const sC = _.last(s).y,
-          eC = _.last(e).y,
-          iC = _.last(i).y,
-          rC = _.last(r).y,
-          s2e = sC * iC * inf / n,
-          e2i = eC / inc,
-          i2r = iC / rec;
-    s.push({ x, y: sC - s2e });
-    e.push({ x, y: eC + s2e - e2i });
-    i.push({ x, y: iC + e2i - i2r });
-    r.push({ x, y: rC + i2r });
-  }
-  return [ s, e, i, r ];
-};
+((_, Chart, moment) => {
+  const generateModel = (inf, inc, rec) => {
+    const n = 100,
+          s = [{ x: 0, y: 100 }],
+          e = [{ x: 0, y: 0 }],
+          i = [{ x: 0, y: 1 }],
+          r = [{ x: 0, y: 0 }];
 
-window.onLoad = () => {
-  const [ s, e, i, r ] = generateModel(0.325, 7, 7),
-    green = 'rgb(75, 192, 192)',
-    yellow = 'rgb(255, 205, 86)',
-    red = 'rgb(255, 99, 132)',
-    blue = 'rgb(54, 162, 235)';
+    for (let x = 1; x < 200; x++) { // TODO run until i = 0
+      const sC = _.last(s).y,
+        eC = _.last(e).y,
+        iC = _.last(i).y,
+        rC = _.last(r).y,
+        s2e = sC * iC * inf / n,
+        e2i = eC / inc,
+        i2r = iC / rec;
+      s.push({ x, y: sC - s2e });
+      e.push({ x, y: eC + s2e - e2i });
+      i.push({ x, y: iC + e2i - i2r });
+      r.push({ x, y: rC + i2r });
+    }
 
-  window.lineChart = new Chart(document.getElementById('lineChart'), {
+    return [ s, e, i, r ];
+  };
+
+  const ctx = document.getElementById('lineChart');
+
+  const chartConfig = {
     type: 'line',
     data: {
-      datasets: [
-        {
-          label: 'Susceptible',
-          data: s,
-          fill: false,
-          backgroundColor: green,
-          borderColor: green,
-          pointRadius: 0,
-        },
-        {
-          label: 'Exposed',
-          data: e,
-          fill: false,
-          backgroundColor: yellow,
-          borderColor: yellow,
-          pointRadius: 0,
-        },
-        {
-          label: 'Infected',
-          data: i,
-          fill: false,
-          backgroundColor: red,
-          borderColor: red,
-          pointRadius: 0,
-        },
-        {
-          label: 'Recovered',
-          data: r,
-          fill: false,
-          backgroundColor: blue,
-          borderColor: blue,
-          pointRadius: 0,
-        }
-      ],
+      datasets: [{
+        label: 'Susceptible',
+        fill: false,
+        backgroundColor: 'rgb(75, 192, 192)',
+        borderColor: 'rgb(75, 192, 192)',
+        pointRadius: 0,
+      }, {
+        label: 'Exposed',
+        fill: false,
+        backgroundColor: 'rgb(255, 205, 86)',
+        borderColor: 'rgb(255, 205, 86)',
+        pointRadius: 0,
+      }, {
+        label: 'Infected',
+        fill: false,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        pointRadius: 0,
+      }, {
+        label: 'Recovered',
+        fill: false,
+        backgroundColor: 'rgb(54, 162, 235)',
+        borderColor: 'rgb(54, 162, 235)',
+        pointRadius: 0,
+      }],
     },
     options: {
       scales: {
@@ -71,11 +60,20 @@ window.onLoad = () => {
           type: 'time',
           distribution: 'series',
           time: {
-            parser: (x) => moment().subtract(2, 'weeks').add(x, 'days'),
+            parser: x => moment().subtract(2, 'weeks').add(x, 'days'),
             unit: 'day',
-          }
+          },
         }],
-      }
+      },
     },
-  });
-};
+  };
+
+  const lineChart = new Chart(ctx, chartConfig);
+
+  const [ s, e, i, r ] = generateModel(0.325, 7, 7);
+  lineChart.data.datasets[0].data = s;
+  lineChart.data.datasets[1].data = e;
+  lineChart.data.datasets[2].data = i;
+  lineChart.data.datasets[3].data = r;
+  lineChart.update();
+})(_, Chart, moment);
